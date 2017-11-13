204d7d70fc7fd892653c0cba269f9ab3b9ddc841
lmx <920014697@qq.com>
2017-11-13 20:54:14 +0800

    增加博客数据和接口

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index.js');
 var users = require('./routes/users.js');
 var goods = require('./routes/goods.js')
+var blogs = require('./routes/blogs.js')
 
 var app = express();
 
@@ -25,7 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/goods', goods)
+app.use('/goods', goods);
+app.use('/blogs', blogs);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- init/init-data.js
+++ init/init-data.js
@@ -1,5 +1,7 @@
 // 初始化数据库
 var initialise = require('./initialise')
-var initAdmin=require('./admin')
+var initAdmin=require('./admin');
+var article=require('./article.js')
 initialise();
-initAdmin()
+initAdmin();
+article();
